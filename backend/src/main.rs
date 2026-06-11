bfd2414d4864f772e23dcb58b9fae5c3fb1931d1
JoacoSlime <JoacoSlime@proton.me>
2026-06-10 21:52:33 -0300

    Fix: Habilitar CORS en development

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -65,7 +65,8 @@ async fn main() {
 
     println!("Construyendo aplicación...");
     let mut cors = CorsLayer::new()
-        .allow_origin("http://localhost:3000".parse::<HeaderValue>().unwrap())
+        .allow_origin("http://localhost:5173".parse::<HeaderValue>().unwrap()) // Development
+        .allow_origin("http://localhost:8080".parse::<HeaderValue>().unwrap()) // WebApp
         .allow_methods([Method::GET, Method::POST, Method::PATCH, Method::DELETE])
         .allow_credentials(true)
         .allow_headers([AUTHORIZATION, ACCEPT, CONTENT_TYPE]);
